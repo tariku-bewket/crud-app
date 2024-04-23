5db34dcf3143605d4de6d79152f7025178c36a74
Tariku <tarikubew@gmail.com>
2024-04-23 16:28:10 +0300

    CRUD operations on product

---
+++ routes/productRoute.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router()
+const Product = require('../models/productModel.js');
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.send(products)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+// Get single product by Id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id)
+    res.send(products)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }   
+})
+
+// Create a product
+router.post('/', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update a product
+router.put('/:id', async (req, res) => {
+  try {
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id)
+
+    res.status(201).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Delete Product
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id)
+    
+    if (!product) {
+      return res.status(404).json({message: "Product not found"})
+    }
+
+    res.status(201).json({message: "Product deleted successfully"})
+    
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Product = require('./models/productModel.js');
+const productRoute = require('./routes/productRoute.js');
+
+const port = process.env.PORT || 5500;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Hello from Node API server');
+});
+
+// Product route
+app.use('/api/products', productRoute)
 
 mongoose
   .connect(process.env.DB_URI)
   .then(() => {
     console.log('Connected to DB successfully');
-    app.listen(3000, console.log('server running on port 3000'));
+    app.listen(port, console.log(`server running on port ${port}`));
   })
   .catch((err) => console.log(err.message, `\nConnection to DB failed.`));
-
-app.get('/', (req, res) => {
-  res.send('Hello from Node API server');
-});
